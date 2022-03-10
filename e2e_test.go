893f1bb2b9de2d4a3f5a47d169458f4eaf5cf59b
Kingmidas74 <kingmidas1992@gmail.com>
2022-03-11 01:06:01 +0300

    add general tests

---
+++ e2e_test.go
@@ -0,0 +1,134 @@
+package gonfiguration
+
+import (
+	"os"
+	"testing"
+)
+
+func TestConfiguration_GetSectionWhenYamlFileOnly(t *testing.T) {
+	builder, err := (&ConfigurationBuilder{}).AddYamlFile("appsettings.yaml")
+	if err != nil {
+		t.Error(err)
+	}
+	config, err := builder.Build()
+	if err != nil {
+		t.Error(err)
+	}
+
+	section, err := config.GetSection("AnotherSettings")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if section["Flag"] == nil {
+		t.Error("Key Flag not found")
+	}
+
+	value := section["Flag"].(string)
+
+	if value == "false" {
+		t.Error("Flag is not correct")
+	}
+}
+
+func TestConfiguration_GetSectionWhenEnvOnly(t *testing.T) {
+
+	os.Setenv("AnotherSettings:Flag", "true")
+	defer os.Unsetenv("AnotherSettings:Flag")
+
+	builder, err := (&ConfigurationBuilder{}).AddEnvironmentVariables()
+	if err != nil {
+		t.Error(err)
+	}
+	config, err := builder.Build()
+	if err != nil {
+		t.Error(err)
+	}
+
+	section, err := config.GetSection("AnotherSettings")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if section["Flag"] == nil {
+		t.Error("Key Flag not found")
+	}
+
+	value := section["Flag"].(string)
+
+	if value == "false" {
+		t.Error("Flag is not correct")
+	}
+}
+
+func TestConfiguration_GetSectionWhenEnvOverrideYaml(t *testing.T) {
+
+	os.Setenv("AnotherSettings:Flag", "false")
+	defer os.Unsetenv("AnotherSettings:Flag")
+
+	builder, err := (&ConfigurationBuilder{}).AddYamlFile("appsettings.yaml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	builder, err = builder.AddEnvironmentVariables()
+	if err != nil {
+		t.Error(err)
+	}
+
+	config, err := builder.Build()
+	if err != nil {
+		t.Error(err)
+	}
+
+	section, err := config.GetSection("AnotherSettings")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if section["Flag"] == nil {
+		t.Error("Key Flag not found")
+	}
+
+	value := section["Flag"].(string)
+
+	if value == "true" {
+		t.Error("Flag is not correct")
+	}
+}
+
+func TestConfiguration_GetSectionWhenYamlOverrideEnv(t *testing.T) {
+
+	os.Setenv("AnotherSettings:Flag", "false")
+	defer os.Unsetenv("AnotherSettings:Flag")
+
+	builder, err := (&ConfigurationBuilder{}).AddEnvironmentVariables()
+	if err != nil {
+		t.Error(err)
+	}
+
+	builder, err = builder.AddYamlFile("appsettings.yaml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	config, err := builder.Build()
+	if err != nil {
+		t.Error(err)
+	}
+
+	section, err := config.GetSection("AnotherSettings")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if section["Flag"] == nil {
+		t.Error("Key Flag not found")
+	}
+
+	value := section["Flag"].(string)
+
+	if value == "false" {
+		t.Error("Flag is not correct")
+	}
+}
